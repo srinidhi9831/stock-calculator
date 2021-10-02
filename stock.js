b2ab24c8256d9e00e0942af5b303eacab25429d7
srinidhi9831 <srinidhi9831@gmail.com>
2021-10-02 14:19:21 +0530

    feat: added validation

--- stock.js
+++ stock.js
@@ -1,19 +1,78 @@
-function getStock(){
-    var price=parseInt(document.getElementById("price").value);
-    var quantity=parseInt(document.getElementById("quantity").value);
-    var currentPrice=parseInt(document.getElementById("currentPrice").value);
-    var total = price*quantity;
-    var currentTotal= currentPrice*quantity;
-    var percent;
-    if(currentTotal>total){
-        percent = ((currentTotal-total)/total)*100
-        document.getElementById("output").innerText="Yay PROFIT\n" + percent+"% up";
-      }
-    else{
-        percent=((total-currentTotal)/total)*100;
-        document.getElementById("output").innerText="opps!! its loss\n" + percent+"% down";
+var initialPrice=document.querySelector("#price");
+var quantity=document.querySelector("#quantity");
+var currentPrice=document.querySelector("#currentPrice");
+var output=document.querySelector("#output");
+var button = document.querySelector("#button");
+//console.log(button);
+//console.log(initialPrice);
+
+button.addEventListener("click",eventHandler);
+
+function eventHandler(){
+  
+   if(validate(initialPrice,quantity,currentPrice))
+      calculateProfitAndLoss(initialPrice,quantity,currentPrice);
+      
+    
+      
+}
+
+
+
+function calculateProfitAndLoss(initialPrice,quantity,currentPrice){
+   var costPrice= (initialPrice.value)*(quantity.value);
+   var sellingPrice=(currentPrice.value)*(quantity.value);
+   if(sellingPrice>costPrice){
+   var profit=getProfit(costPrice,sellingPrice);
+   var profitPercentage= (getProfitPercentage(profit,costPrice));
+   show(`profit is ${profit}, profitpercent is ${profitPercentage}%`);
+   }
+   else if(costPrice>sellingPrice){
+   var loss=getLoss(costPrice,sellingPrice);
+   var lossPercentage=getLossPercentage(loss,costPrice);
+   show(`loss is ${loss}, losspercentage is ${lossPercentage}%`);
+   }
+   else{
+    show(`no loss no profit`);
+   }
+
+}
+
+
+function getProfit(costPrice,sellingPrice){
+     return (sellingPrice-costPrice)
+}
+
+function getLoss(costPrice,sellingPrice){
+    return (costPrice-sellingPrice);
+}
+
+function getProfitPercentage(profit,costPrice){
+     return ((profit/costPrice)*100).toFixed(2);
+}
+
+function getLossPercentage(loss,costPrice){
+    return ((loss/costPrice)*100).toFixed(2);
+}
+
+function show(message){
+    output.innerText=`${message}`;
+    console.log(message);
+}
+  
+function validate(initialPrice,quantity,currentPrice){
+
+    if(initialPrice.value==="" || quantity.value==="" || currentPrice.value===""){
+            alert("please fill out all fields");
+            return 0;
     }
-
-   
-
+    else if(isNaN(initialPrice.value) || isNaN(quantity.value) || isNaN(currentPrice.value)){
+        console.log(`inside val ${isNaN(initialPrice)}`);
+         show("please enter number only");
+         return 0;
+        
+     }
+     else
+       console.log("validate sucess");
+       return true;    
 }
